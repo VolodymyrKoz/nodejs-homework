efc186185c808bc5d945a663c28a6de110b86a29
VolodymyK <vkill7595@gmail.com>
2023-09-08 18:28:55 -0500

    all done

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("./mongoose");
 
-const contactsRouter = require('./routes/api/contacts')
+// Check the mongoose connection status
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const Joi = require("joi"); 
 const router = express.Router();
+const Joi = require("joi");
+const mongoose = require("../mongoose"); // Import the mongoose.js file where the database connection is established.
 
 const contactSchema = Joi.object({
   name: Joi.string().required(),
@@ -8,12 +9,43 @@ const contactSchema = Joi.object({
   phone: Joi.string().required(),
 });
 
+const Contact = mongoose.model("Contact", {
+  // Define your Mongoose Contact model here.
+  name: String,
+  email: String,
+  phone: String,
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// GET all contacts
 router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
 });
 
+// GET a specific contact by ID
 router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const contact = await Contact.findById(contactId);
+    if (!contact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
 });
 
+// POST a new contact
 router.post("/", async (req, res, next) => {
   try {
     const { error } = contactSchema.validate(req.body);
@@ -22,16 +54,34 @@ router.post("/", async (req, res, next) => {
       return res.status(400).json({ message: error.details[0].message });
     }
 
+    const newContact = new Contact(req.body);
+    await newContact.save();
+
     res.json({ message: "Contact added successfully" });
   } catch (error) {
     next(error);
   }
 });
 
+// DELETE a contact by ID
 router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const deletedContact = await Contact.findByIdAndRemove(contactId);
+    if (!deletedContact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+    res.json({ message: "Contact deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
 });
 
+// PUT (update) a contact by ID
 router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
   try {
     const { error } = contactSchema.validate(req.body);
 
@@ -39,7 +89,17 @@ router.put("/:contactId", async (req, res, next) => {
       return res.status(400).json({ message: error.details[0].message });
     }
 
-    res.json({ message: "Contact updated successfully" });
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    res.json({ message: "Contact updated successfully", updatedContact });
   } catch (error) {
     next(error);
   }
